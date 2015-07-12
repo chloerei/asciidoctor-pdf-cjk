8e18f4a23ece9a98f3afee5c41e2fb8c5b015ce1
Rei <chloerei@gmail.com>
2015-07-12 16:10:46 +0800

    insert ZWSP before Han

--- lib/asciidoctor/pdf/cjk/converter.rb
+++ lib/asciidoctor/pdf/cjk/converter.rb
@@ -2,7 +2,7 @@ module Asciidoctor
   module Pdf
     class Converter
       def typeset_text_with_break_word(string, line_metrics, opts = {})
-        typeset_text_without_break_word string.gsub(/[\p{Han}\p{Hiragana}\p{Katakana}]/) {|s| "#{s}#{::Prawn::Text::ZWSP}"}, line_metrics, opts
+        typeset_text_without_break_word string.gsub(/(?<!\p{Space})[\p{Han}\p{Hiragana}\p{Katakana}]/) {|s| "#{::Prawn::Text::ZWSP}#{s}"}, line_metrics, opts
       end
       alias_method :typeset_text_without_break_word, :typeset_text
       alias_method :typeset_text, :typeset_text_with_break_word
