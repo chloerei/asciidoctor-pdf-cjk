cd1984d33ca09db07f289fcb3551c5b219a56740
Rei <chloerei@gmail.com>
2015-10-17 16:48:02 +0800

    break words consider punctuation

--- lib/asciidoctor/pdf/cjk/converter.rb
+++ lib/asciidoctor/pdf/cjk/converter.rb
@@ -1,11 +1,11 @@
 module Asciidoctor
   module Pdf
     class Converter
-      def typeset_text_with_break_word(string, line_metrics, opts = {})
-        typeset_text_without_break_word string.gsub(/(?<!\p{Space})[\p{Han}\p{Hiragana}\p{Katakana}]/) {|s| "#{::Prawn::Text::ZWSP}#{s}"}, line_metrics, opts
+      def typeset_text_with_break_words(string, line_metrics, opts = {})
+        typeset_text_without_break_words ::Asciidoctor::Pdf::Cjk.break_words(string), line_metrics, opts
       end
-      alias_method :typeset_text_without_break_word, :typeset_text
-      alias_method :typeset_text, :typeset_text_with_break_word
+      alias_method :typeset_text_without_break_words, :typeset_text
+      alias_method :typeset_text, :typeset_text_with_break_words
     end
   end
 end
--- test/asciidoctor/pdf/cjk_test.rb
+++ test/asciidoctor/pdf/cjk_test.rb
@@ -4,4 +4,12 @@ class Asciidoctor::Pdf::CjkTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Asciidoctor::Pdf::Cjk::VERSION
   end
+
+  def test_break_words_chinese
+    zwsp = Prawn::Text::ZWSP
+    assert_equal "测#{zwsp}试", Asciidoctor::Pdf::Cjk.break_words("测试")
+    assert_equal "测#{zwsp}试#{zwsp}（括#{zwsp}号）", Asciidoctor::Pdf::Cjk.break_words("测试（括号）")
+    assert_equal "测#{zwsp}试#{zwsp}“引#{zwsp}号”", Asciidoctor::Pdf::Cjk.break_words("测试“引号”")
+    assert_equal "逗#{zwsp}号，#{zwsp}句#{zwsp}号。", Asciidoctor::Pdf::Cjk.break_words("逗号，句号。")
+  end
 end
